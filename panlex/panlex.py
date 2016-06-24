b17c0193c753fd8e058f07fce6b26fcf972408f4
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-06-24 14:18:31 -0700

    generalized the translation method to any language (uses UIDs)

--- panlex/panlex.py
+++ panlex/panlex.py
@@ -8,10 +8,12 @@ def extractResult(json, field):
 
 # limited to 2 calls/second
 @rate_limited(2)
-def translateFromEnglishToMandarin(targetWord):
+def translate(expn, startLang, endLang):
+    """Get the best-quality translation of expn, an expression in startLang, into endLang.
+    Languages are specified as PanLex UID codes (e.g. eng-000 for English.)"""
     url = "http://api.panlex.org/ex"
-    ps = {"uid":"eng-000",
-          "tt":targetWord,
+    ps = {"uid":startLang,
+          "tt":expn,
           "indent":True}
     r1 = rq.post(url, data=json.dumps(ps))
     if r1.status_code != rq.codes.ok:
@@ -19,7 +21,7 @@ def translateFromEnglishToMandarin(targetWord):
     else:
         exid = extractResult(r1.json(),"ex")
         r2 = rq.post(url, json.dumps({"trex":exid,
-                                      "uid":"cmn-000",
+                                      "uid":endLang,
                                       "indent":True,
                                       "include":"trq",
                                       "sort":"trq desc",
@@ -30,7 +32,7 @@ def translateFromEnglishToMandarin(targetWord):
             return extractResult(r2.json(),"tt")
 
 def main():
-    print(translateFromEnglishToMandarin("tree"))
+    print(translate("tree","eng-000","cmn-000"))
 
 if __name__ == "__main__":
     main()
