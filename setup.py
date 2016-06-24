5b8afacfae54bb6554a0612cbdd960a14eddefab
Caroline <carolineroseglazer@gmail.com>
2016-06-24 12:26:43 -0700

    added setup.py for PyPI

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panlex_API',
+    version='1.0.0',
+    description='Python wrapper for PanLex API',
+    install_requires=['ratelimit','requests']
+)
