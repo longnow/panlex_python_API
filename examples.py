e181b43d6c16e083a3679af43f3dcd13b94318ed
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-06-29 09:02:22 -0700

    moved translate function out into an examples file, since it's not part of the API

---
+++ examples.py
@@ -0,0 +1,18 @@
+# example uses of the PanLex Python API
+
+def translate(expn, startLang, endLang):
+    """Get the best-quality translation of expn, an expression in startLang, into endLang.
+    Languages are specified as PanLex UID codes (e.g. eng-000 for English.)"""
+    params1 = {"uid":startLang,
+               "tt":expn,
+               "indent":True}
+    r1 = query("ex",params1)
+    exid = extractResult(r1,"ex")
+    params2 = {"trex":exid,
+               "uid":endLang,
+               "indent":True,
+               "include":"trq" # include the field trq, "translation quality",
+               "sort":"trq desc" # sort by descending trq,
+               "limit":1, # get only one result (which will be the one with the highest score)}
+    r2 = query("ex",params2)
+    return extractResult(r2,"tt")
--- panlex.py
+++ panlex.py
@@ -75,20 +75,3 @@ def queryNorm(ep, params):
         if start > len(params["tt"]):
             break
     return retVal
-
-def translate(expn, startLang, endLang):
-    """Get the best-quality translation of expn, an expression in startLang, into endLang.
-    Languages are specified as PanLex UID codes (e.g. eng-000 for English.)"""
-    params1 = {"uid":startLang,
-               "tt":expn,
-               "indent":True}
-    r1 = query("ex",params1)
-    exid = extractResult(r1,"ex")
-    params2 = {"trex":exid,
-               "uid":endLang,
-               "indent":True,
-               "include":"trq",
-               "sort":"trq desc",
-               "limit":1}
-    r2 = query("ex",params2)
-    return extractResult(r2,"tt")
