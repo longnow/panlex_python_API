1e7da65759d9f5a4726d8391f4311267b97a8d01
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-06-29 16:38:53 -0700

    fixed typos in arguments to query

--- examples.py
+++ examples.py
@@ -1,4 +1,5 @@
 # example uses of the PanLex Python API
+from panlex import *
 
 def translate(expn, startLang, endLang):
     """Get the best-quality translation of expn, an expression in startLang, into endLang.
@@ -6,13 +7,13 @@ def translate(expn, startLang, endLang):
     params1 = {"uid":startLang,
                "tt":expn,
                "indent":True}
-    r1 = query("ex",params1)
+    r1 = query("/ex",params1)
     exid = extractResult(r1,"ex")
     params2 = {"trex":exid,
                "uid":endLang,
                "indent":True,
-               "include":"trq" # include the field trq, "translation quality",
-               "sort":"trq desc" # sort by descending trq,
-               "limit":1, # get only one result (which will be the one with the highest score)}
-    r2 = query("ex",params2)
+               "include":"trq", # include the field trq, "translation quality",
+               "sort":"trq desc", # sort by descending trq,
+               "limit":1} # get only one result (which will be the one with the highest score)}
+    r2 = query("/ex",params2)
     return extractResult(r2,"tt")
