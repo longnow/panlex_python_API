623a84a47683913a2848005aa126dd26b5c31ebf
maxwelljoslyn <maxwelljoslyn@gmail.com>
2016-07-28 15:33:16 -0700

    added more metadata used by PyPI

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='panlex_API',
     version='1.0.0',
+    author="Maxwell Joslyn; Caroline Glazer"
+    author_email="",
+    url="panlex.org",
     description='Python wrapper for PanLex API',
-    install_requires=['ratelimit','requests']
+    install_requires=['ratelimit','requests'],
+    classifiers=["Development Status :: 5 - Production/Stable", "Programming Language :: Python",
+                 "Programming Language :: Python :: 3", "Operating System :: OS Independent",
+                 "License :: OSI Approved :: MIT License", "Topic :: Software Development :: Libraries :: Python Modules"]
 )
